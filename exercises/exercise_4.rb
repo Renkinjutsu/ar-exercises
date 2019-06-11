b20c90dc9446cb33d3f9a501ab867a745c629b8d
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-11 13:55:34 -0400

    did exercises 1-6

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,35 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(
+    name: "Surrey", 
+    annual_revenue: 300000, 
+    mens_apparel: true, 
+    womens_apparel: true
+)
+
+whistler = Store.create(
+    name: "Whistler", 
+    annual_revenue: 190000, 
+    mens_apparel: false, 
+    womens_apparel: true
+)
+
+yaletown = Store.create(
+    name: "Yaletown", 
+    annual_revenue: 1260000, 
+    mens_apparel: true, 
+    womens_apparel: false
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+    puts store
+end
+
+@women_baller_stores = Store.where('womens_apparel = true AND annual_revenue > 1000000')
+
+@women_baller_stores.each do |store|
+    puts store
+end
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mhurram", last_name: "Mirani", hourly_rate: 30)
+@store1.employees.create(first_name: "Shurram", last_name: "Sirani", hourly_rate: 40)
+@store1.employees.create(first_name: "Bhurram", last_name: "Birani", hourly_rate: 600)
+
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Mhurram", last_name: "Mirani", hourly_rate: 30)
+@store2.employees.create(first_name: "Shurram", last_name: "Sirani", hourly_rate: 40)
+@store2.employees.create(first_name: "Bhurram", last_name: "Birani", hourly_rate: 600)
